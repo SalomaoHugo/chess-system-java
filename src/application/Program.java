7fce1269f895449862722386cc4a466a273fc475
Salomão Oliveira <salomaohugo@gmail.com>
2021-03-16 09:26:23 -0300

    first class position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Anja is a muppet");
+		
+		Position pos = new Position (3, 5);
+		System.out.println(pos);
 	}
 
 }
